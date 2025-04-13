92f1f57a8c45bbd7072636acef5639db319cdccc
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-04-13 18:30:30 +0200

    create Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello, Aziz</h1>
+      <Navbar />
     </>
   )
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+export default function Navbar() {
+
+return (
+    <>
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start">
+                <a className="btn btn-ghost text-xl">Jobbando</a>
+            </div>
+            <div className="navbar-center">
+                <input type="text" placeholder="Search" className="input input-bordered w-48 md:w-auto" />
+            </div>
+            <div className="navbar-end">
+                <a className="btn btn-primary">Add</a>
+            </div>
+        </div>
+    </>
+)
+
+}
