b2e5b537ca560c1fe4b31f40dfad7e6b9cf00b35
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-04-13 18:16:52 +0200

    use DaisyUi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello, Aziz</h1>
+      <h1 className="text-3xl font-bold underline">Hello, Aziz</h1>
     </>
   )
 }
