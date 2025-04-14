ab3226f70cd5ee80553bc671d72999b53a4f2516
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-04-14 17:20:48 +0200

    start modalForm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,30 @@
 import './App.css'
+import ModalForm from './components/ModalForm'
 import Navbar from './components/Navbar'
 import TableList from './components/Tablelist'
+import { useState } from 'react';
 
 function App() {
 
+  const [IsOpen, setIsOpen] = useState(false);
+  const [modalMode, setmodalMode] = useState('add');
+
+  const handleOpen = (mode) => {
+    setIsOpen(true);
+  }
+
+  const handleSubmit = () => {
+      if (modalMode === 'add') {
+        console.log('modal mode add:');
+      } else {
+        console.log('modal mode edit:');
+      }
+  }
   return (
     <>
-      <Navbar />
+      <Navbar onOpen = {() => handleOpen('add')}/>
       <TableList />
+      <ModalForm isOpen={(isOpen)} onSubmit={(handleSubmit)} onClose={() => setIsOpen(false)} />
     </>
   )
 }
