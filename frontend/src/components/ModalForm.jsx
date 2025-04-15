cfa6020df67d14b5a43d497b958ce6c7dd6cd90d
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-04-15 11:25:14 +0200

    done Frontend

--- frontend/src/components/ModalForm.jsx
+++ frontend/src/components/ModalForm.jsx
@@ -1,43 +1,63 @@
+import { useState } from 'react';
+
 export default function ModalForm({isOpen, onClose, mode, onSubmit}) {
+
+    const [company, setCompany] = useState('');
+    const [position, setPosition] = useState('');
+    const [location, setLocation] = useState('');
+    const [date, setDate] = useState('');
+    const [status, setStatus] = useState('');
+    const [link, setLink] = useState('');
+
+    const handleStatusChange = (e) => {
+        setStatus(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSubmit({ company, position, location, date, status, link });
+        onClose(); // Close the modal after submission
+    }
 
     return (
         <>
             <dialog id="my_modal_3" className="modal" open={isOpen}>
             <div className="modal-box">
                 <h3 className="font-bold text-lg py-4 text-center">{mode === 'edit' ? 'Edit' : 'New Application'}</h3>
-                <form method="dialog">
+
+                <form method="dialog" onSubmit={handleSubmit}>
                     <div className="flex mb-4 justify-between">
                         <label className="flex input-bordered items-center gap-2 mr-1 input my-4">
-                            <input type="text" className="grow" placeholder="Azienda" />
+                            <input type="text" className="grow" placeholder="Azienda" value={company} onChange={(e) => setCompany(e.target.value)} />
                         </label>
                         <label className="flex input-bordered items-center gap-2 input my-4">
-                            <input type="text" className="grow" placeholder="Posizione" />
+                            <input type="text" className="grow" placeholder="Posizione"  value={position} onChange={(e) => setPosition(e.target.value)}/>
                         </label>
                     </div>
 
                         Data di candidatura:
                     <label className="flex items-center gap-2 input mb-4 w-full">
-                        <input type="date"/>
+                        <input type="date" value={date} onChange={(e) => setDate(e.target.value)}/>
                     </label>
 
                     <div className="flex mb-4 justify-between">
                         <label className="flex items-center gap-2 input my-4">
-                            <input type="text" className="grow" placeholder="Luogo" />
+                            <input type="text" className="grow" placeholder="Luogo"  value={location} onChange={(e) => setLocation(e.target.value)}/>
                         </label>
                         <label className="flex items-center gap-2 m-2">
-                            <select defaultValue="Candidatura inviata" className="select">
-                                <option disabled={true}>Stato</option>
-                                <option>Candidatura inviata</option>
-                                <option>Colloquio programmato</option>
-                                <option>Offerta ricevuta</option>
-                                <option>Rifiutato</option>
+                            <select value={status} onChange={handleStatusChange} className="select">
+                                <option value="" disabled >Stato</option>
+                                <option value="Candidatura inviata">Candidatura inviata</option>
+                                <option value="Colloquio programmato">Colloquio programmato</option>
+                                <option value="Offerta ricevuta">Offerta ricevuta</option>
+                                <option value="Rifiutato">Rifiutato</option>
                             </select>
                         </label>
 
                     </div>
                     <label className="flex items-center gap-2 input my-4 w-full">
                         <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></g></svg>
-                        <input type="url" placeholder="- Link Offerta -" />
+                        <input type="url" placeholder="- Link Offerta -"  value={link} onChange={(e) => setLink(e.target.value)}/>
                     </label>
                     <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={onClose}>✕</button>
                     <button className="btn btn-success w-full" onClick={onSubmit}>{mode === 'edit' ? 'Save Changes' : 'Create'}</button>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,10 @@ return (
                 <a className="btn btn-ghost text-xl">Jobbando</a>
             </div>
             <div className="navbar-center">
-                <input type="text" placeholder="Search" className="input input-bordered w-48 md:w-auto" />
+                <label className="input">
+                    <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g></svg>
+                    <input type="search" className="grow" placeholder="Search" />
+                </label>
             </div>
             <div className="navbar-end">
                 <a className="btn btn-primary" onClick={onOpen}>Aggiungi una candidatura</a>
